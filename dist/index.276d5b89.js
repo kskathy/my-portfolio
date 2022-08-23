0ce47c5898a8a148c550ae20c434c4f38224a70b
Kathy <kskathy123@yahoo.com>
2022-08-22 22:38:12 -0400

    contact added

--- dist/index.276d5b89.js
+++ dist/index.276d5b89.js
@@ -554,6 +554,26 @@ document.addEventListener("DOMContentLoaded", function() {
     //     }
     //   }
     //   window.addEventListener("scroll", reveal);
+    // MODAL ANIMATION
+    const modal = document.querySelector("#my-modal");
+    const modalBtn = document.querySelector("#modal-btn");
+    const closeBtn = document.querySelector(".close");
+    // Events
+    modalBtn.addEventListener("click", openModal);
+    closeBtn.addEventListener("click", closeModal);
+    window.addEventListener("click", outsideClick);
+    // Open
+    function openModal() {
+        modal.style.display = "block";
+    }
+    // Close
+    function closeModal() {
+        modal.style.display = "none";
+    }
+    // Close If Outside Click
+    function outsideClick(e) {
+        if (e.target == modal) modal.style.display = "none";
+    }
     // TEXT ANIMATION
     let fadeupCallback = (entries)=>{
         entries.forEach((entry)=>{
--- src/js/image.js
+++ src/js/image.js
@@ -26,7 +26,32 @@ document.addEventListener("DOMContentLoaded", function () {
     //   window.addEventListener("scroll", reveal);
 
       
+// MODAL ANIMATION
+const modal = document.querySelector('#my-modal');
+const modalBtn = document.querySelector('#modal-btn');
+const closeBtn = document.querySelector('.close');
 
+// Events
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+// Open
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Close
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// Close If Outside Click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
       
     // TEXT ANIMATION
     let fadeupCallback = (entries) => {
